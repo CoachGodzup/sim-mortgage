03c502e53234cb6d29b9cb5e24651860156cff46
Matteo Garza <matteo.garza@facile.it>
2021-06-16 00:05:08 +0200

    chore: ranking component

---
+++ src/pages/result/ranking.tsx
@@ -0,0 +1,12 @@
+export default function Ranking() {
+  return <div>
+    <h2>Ranking (after 30 turns)</h2>
+    <ol>
+      <li>Hin Xiaomi - 15000 pts</li>
+      <li>Thomas Santoku - 14253 pts</li>
+      <li>Gino Pilotino - 12321 pts</li>
+      <li>Marcus Embed - 10223 pts</li>
+      <li>Robert Capa - 9432 pts</li>
+    </ol>
+  </div>
+}
