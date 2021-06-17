a3c826438a8f95fd519329bdccc6e04a0ee39661
Matteo Garza <matteo.garza@facile.it>
2021-06-17 17:52:07 +0200

    chore: form new game with player form

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -2,24 +2,23 @@ import { route } from "preact-router";
 import { PageParams } from "../models/page";
 
 export default function Player(_: PageParams) {
-
   const submitHandler = (event: Event) => {
     event.preventDefault();
     // action : create player
     // action : add player to match
 
-    route('/match');
-  }
+    route("/match");
+  };
 
   return (
     <section>
       <form onSubmit={submitHandler}>
         <div className="inputGroup">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="name">Nickname</label>
           <input type="text" name="name" />
         </div>
       </form>
       <button type="submit">Let me in!</button>
     </section>
-  )
+  );
 }
